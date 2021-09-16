70feddcf3707ce20e743b22639940a1215724427
Alex Siqueira Lacerda <Lequizi@gmail.com>
2021-09-16 18:14:09 -0300

    Update production.py

--- voluntario/settings/production.py
+++ voluntario/settings/production.py
@@ -19,7 +19,7 @@ DATABASES = {
     
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR,'sqlite3.db')
+    #    'NAME': os.path.join(BASE_DIR,'db.sqlite3')
     #}
 
 
